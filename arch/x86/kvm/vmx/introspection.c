2446089d0a390f153642ae4317ba3739bdf5d9a4
rose <47357290+rose4096@users.noreply.github.com>
2022-02-03 23:29:28 -0500

    fix onet hing , break another ..

--- arch/x86/kvm/vmx/introspection.c
+++ arch/x86/kvm/vmx/introspection.c
@@ -57,18 +57,38 @@
 #define ZYAN_NO_LIBC
 #include <Zydis/Zydis.h>
 
+#define DEFINE_MEMORY_OPERATION(name, type) \
+	type intro_read_virt##name(struct kvm_vcpu* vcpu, gva_t addr) {  \
+        	type ret;						 \
+		intro_read_virt(vcpu, addr, &ret, sizeof(type));	 \
+        	return ret; 						 \
+	}								 \
+									 \
+	void intro_write_virt##name(struct kvm_vcpu* vcpu, gva_t addr, type value) { \
+		intro_write_virt(vcpu, addr, &value, sizeof(type));		     \
+	}                                                               \
+                                                                        \
+	type intro_read_phys##name(struct kvm_vcpu* vcpu, gpa_t addr) { \
+        	type ret;						 \
+		intro_read_phys(vcpu, addr, &ret, sizeof(type));	 \
+        	return ret; 						 \
+	}								 \
+									 \
+	void intro_write_phys##name(struct kvm_vcpu* vcpu, gpa_t addr, type value) { \
+		intro_write_phys(vcpu, addr, &value, sizeof(type));		     \
+	}
+
+
 struct windows_context {
 	u64 PsLoadedModuleList;
 };
 
+struct windows_pe {
+
+};
+
 typedef void(*disassemble_callback_t)(ZydisDecodedInstruction*, u64);
 typedef void(*disassemble_text_callback_t)(ZydisDecodedInstruction*, const char*, u64);
-
-u64 intro_init_win_context(struct windows_context* ctx);
-u64 intro_init_pe(void);
-
-u64 intro_get_ntoskrnl_entrypoint(struct kvm_vcpu* vcpu);
-u64 intro_get_ntoskrnl(struct kvm_vcpu* vcpu);
 
 void intro_disassemble(hva_t addr, gva_t rip, u32 len, disassemble_callback_t callback) {
 	ZydisDecoder decoder;
@@ -112,82 +132,104 @@ void intro_disassemble_text(hva_t addr, gva_t rip, u32 len, disassemble_text_cal
 	}
 }
 
+void intro_read_virt(struct kvm_vcpu* vcpu, gva_t addr, void* buffer, u32 len) {
+	struct x86_exception e;
+	kvm_read_guest(vcpu->kvm, kvm_mmu_gva_to_gpa_system(vcpu, addr, &e), buffer, len);
+}
+
+void intro_write_virt(struct kvm_vcpu* vcpu, gva_t addr, void* buffer, u32 len) {
+	struct x86_exception e;
+	kvm_write_guest(vcpu->kvm, kvm_mmu_gva_to_gpa_system(vcpu, addr, &e), buffer, len);
+}
+
+void intro_read_phys(struct kvm_vcpu* vcpu, gpa_t addr, void* buffer, u32 len) {
+	kvm_read_guest(vcpu->kvm, addr, buffer, len);
+}
+
+void intro_write_phys(struct kvm_vcpu* vcpu, gpa_t addr, void* buffer, u32 len) {
+	kvm_write_guest(vcpu->kvm, addr, buffer, len);
+}
+
+hva_t intro_map_virt(struct kvm_vcpu *vcpu, gva_t addr, struct kvm_host_map* map) {
+	gpa_t gpa = kvm_mmu_gva_to_gpa_read(vcpu, addr, NULL);
+	if (kvm_vcpu_map(vcpu, gpa_to_gfn(gpa), map))
+		return 0;
+
+	if (!map->hva)
+		return 0;
+
+	return (hva_t)(map->hva + offset_in_page(gpa));
+}
+
+void intro_unmap(struct kvm_vcpu *vcpu, struct kvm_host_map* map) {
+	kvm_vcpu_unmap(vcpu, map, true);
+}
+
+DEFINE_MEMORY_OPERATION(64, u64);
+DEFINE_MEMORY_OPERATION(32, u32);
+DEFINE_MEMORY_OPERATION(16, u16);
+DEFINE_MEMORY_OPERATION(8, u8);
+
+bool intro_init_win_context(struct windows_context* ctx);
+bool intro_init_pe(struct windows_pe* pe, gva_t address);
+
+/* https://github.com/ufrisk/MemProcFS/blob/fd9aa0219b29c5916b8f036ee3b1e0d1f486e14d/vmm/vmmwininit.c#L597 */
+gva_t intro_get_ntoskrnl_entrypoint(struct kvm_vcpu* vcpu) {
+	u8 buf[0x1000];
+	for (u32 index = 0; index < 100; index++) {
+		memset(buf, 0, sizeof(buf));
+
+		gpa_t addr = index * 0x1000;
+		intro_read_phys(vcpu, addr, buf, 0x1000);
+
+		u64 start_bytes = *(u64*)(buf);
+		if (0x00000001000600E9 ^ (0xffffffffffff00ff & start_bytes))
+			continue;
+
+		gva_t kernel_entrypoint = *(gva_t*)(buf + 0x70);
+		if (0xfffff80000000000 ^ (0xfffff80000000000 & kernel_entrypoint))
+			continue;
+
+		u64 pml4 = *(u64*)(buf + 0xa0);
+		if (0xffffff0000000fff & pml4)
+			continue;
+
+		return kernel_entrypoint;
+	}
+
+	return 0;
+}
+
+gva_t intro_get_ntoskrnl(struct kvm_vcpu* vcpu) {
+	gva_t entrypoint = intro_get_ntoskrnl_entrypoint(vcpu) & ~( PAGE_SIZE - 1 );
+
+	for ( u64 index = 0; index < 0x1000000 && entrypoint > 0; index++ ) {
+		u16 mz_header = intro_read_virt16(vcpu, entrypoint);
+		if (mz_header != 0x5A4D)
+			goto next_page;
+
+		u32 e_lfanew = intro_read_virt32(vcpu, entrypoint + 0x3C);
+		if (e_lfanew <= 0x40 || e_lfanew > 0x200 )
+			goto next_page;
+
+		u32 nt_signature = intro_read_virt32(vcpu, entrypoint + e_lfanew);
+		if (nt_signature != 0x4550)
+			goto next_page;
+
+		u32 image_size = intro_read_virt32(vcpu, entrypoint + e_lfanew + 0x50);
+		if (image_size < 0x1000000)
+			goto next_page;
+
+		return entrypoint;
+
+	next_page:
+		entrypoint -= PAGE_SIZE;
+	}
+
+	return 0;
+}
+
 //static u64 g_psloadedmodulelist = 0;
-//
-//u64 get_kernel_entry(struct kvm_vcpu* vcpu) {
-//	u8 buf[ 0x1000 ];
-//
-//	for ( u32 i = 0; i < 100; i++ ) {
-//		memset( buf, 0, sizeof( buf ) );
-//
-//		u64 addr = i * 0x1000;
-//		struct kvm_host_map map;
-//		if (kvm_vcpu_map(vcpu, gpa_to_gfn(addr), &map)) {
-//			continue;
-//		}
-//
-//		if (!map.hva) {
-//			continue;
-//		}
-//
-//		memcpy(buf, map.hva, 0x1000);
-//
-//		kvm_vcpu_unmap(vcpu, &map, true);
-//
-//		if ( 0x00000001000600E9 ^ ( 0xffffffffffff00ff & *( u64* ) ( buf ) ) ) {
-//			continue;
-//		}
-//
-//		if ( 0xfffff80000000000 ^ ( 0xfffff80000000000 & *( u64* ) ( buf + 0x70 ) ) ) {
-//			continue;
-//		}
-//
-//		if ( 0xffffff0000000fff & *( u64* ) ( buf + 0xa0 ) ) {
-//			continue;
-//		}
-//
-//		return *( u64* ) ( buf + 0x70 );
-//	}
-//
-//	return 0;
-//}
-//
-//u64 get_kernel_base_(struct kvm_vcpu* vcpu, u64 address) {
-//	printk(KERN_ALERT "address [0x%llx]\n", address);
-//
-//	u64 base = 0;
-//	address &= ~( PAGE_SIZE - 1 );
-//
-//	for ( u64 size = 0; size < (1024 * 1024) && address > 0; size += 1 ) {
-//		u16 header = 0;
-//		struct x86_exception e;
-//		kvm_read_guest(vcpu->kvm, kvm_mmu_gva_to_gpa_system(vcpu, address, &e), &header, sizeof(u16));
-//
-//		if (header == 0x5A4D) {
-//			u32 eflaw = 0;
-//			kvm_read_guest(vcpu->kvm, kvm_mmu_gva_to_gpa_system(vcpu, address + 0x3C, &e), &eflaw, sizeof(u32));
-//
-//			if (eflaw >= 0x40 && eflaw < 0x200 ) {
-//				u32 sus = 0;
-//				kvm_read_guest(vcpu->kvm, kvm_mmu_gva_to_gpa_system(vcpu, address + eflaw, &e), &sus, sizeof(u32));
-//				if (sus == 0x4550) {
-//					u32 size = 0;
-//					kvm_read_guest(vcpu->kvm, kvm_mmu_gva_to_gpa_system(vcpu, address + eflaw + 0x50, &e), &size, sizeof(u32));
-//					printk(KERN_ALERT "sussy image size -> [0x%llx] 0x%x\n", address, size);
-//
-//					if (size >= 0x1000000) {
-//						base = address;
-//						break;
-//					}
-//				}
-//			}
-//		}
-//
-//		address -= PAGE_SIZE;
-//	}
-//
-//	return base;
-//}
 
 /*char* utf16toutf8_(char* Destination, const u16* Source, u32 DestinationMaxLength)
 {
@@ -408,21 +450,6 @@ void introspection_xsetbv_callback(struct kvm_vcpu *vcpu) {
 //	slide_with_the_heater(vcpu);
 }
 
-hva_t intro_map_virt(struct kvm_vcpu *vcpu, gva_t addr, struct kvm_host_map* map) {
-	gpa_t gpa = kvm_mmu_gva_to_gpa_read(vcpu, addr, NULL);
-	if (kvm_vcpu_map(vcpu, gpa_to_gfn(gpa), map))
-		return 0;
-
-	if (!map->hva)
-		return 0;
-
-	return (hva_t)(map->hva + offset_in_page(gpa));
-}
-
-void intro_unmap(struct kvm_vcpu *vcpu, struct kvm_host_map* map) {
-	kvm_vcpu_unmap(vcpu, map, true);
-}
-
 void introspection_cpuid_callback(struct kvm_vcpu *vcpu) {
 	struct kvm_host_map map;
 	u32 eax = kvm_rax_read(vcpu);
@@ -434,8 +461,13 @@ void introspection_cpuid_callback(struct kvm_vcpu *vcpu) {
 				printk(KERN_ALERT "0x%llx %s\n", rip, text);
 			}
 
-			intro_disassemble_text(intro_map_virt(vcpu, rip, &map), rip, 0x20, cb);
-			intro_unmap(vcpu, &map);
+			hva_t mapping = intro_map_virt(vcpu, rip, &map);
+			if (mapping) {
+				intro_disassemble_text(intro_map_virt(vcpu, rip, &map), rip, 0x20, cb);
+				intro_unmap(vcpu, &map);
+			}
+
+			printk(KERN_ALERT "0x%llx\n", intro_get_ntoskrnl(vcpu));
 		}
 	}
 
@@ -501,38 +533,71 @@ void introspection_cpuid_callback(struct kvm_vcpu *vcpu) {
 //	}
 }
 
+void battleye_anti_vm(struct kvm_vcpu* vcpu) {
+	struct kvm_host_map map;
+	u64 rip = kvm_rip_read(vcpu);
+	u64 r9 = kvm_r9_read(vcpu);
+
+	if (r9 == 0x6590) {
+		printk(KERN_ALERT "[0x%llx] r9 == 0x6590, setting r9 to 10...\n", rip);
+		kvm_r9_write(vcpu, 10);
+	}
+
+	u8 vm_cmp[7] = {
+		// cmp r9d, 0x6590
+		0x41, 0x81, 0x9, 0x90, 0x65, 0x00, 0x00
+	};
+
+	hva_t rip_hva = intro_map_virt(vcpu, rip - 0x20, &map);
+	if (!rip_hva)
+		return;
+
+	for (u64 offset = 0; offset < 0x20; offset++) {
+		if (!memcmp(rip_hva + offset, vm_cmp, sizeof(vm_cmp))) {
+			printk(KERN_ALERT "[0x%llx] found cmp, setting r9 to 0x6590...\n", rip);
+			kvm_r9_write(vcpu, 0x6590);
+		}
+	}
+
+	intro_unmap(vcpu, &map);
+}
+
+void anti_vm_real(struct kvm_vcpu* vcpu) {
+	u64 rip = kvm_rip_read(vcpu) - 0x20;
+	u64 r9 = kvm_r9_read(vcpu);
+
+	struct kvm_host_map map;
+	gpa_t gpa = kvm_mmu_gva_to_gpa_read(vcpu, rip, NULL);
+	if (kvm_vcpu_map(vcpu, gpa_to_gfn(gpa), &map)) {
+		printk(KERN_ALERT "vcpu map fail...\n");
+		return;
+	}
+
+	if (!map.hva) {
+		printk(KERN_ALERT "mapped page invalid hva...\n");
+		return;
+	}
+
+	u8* hva = map.hva + offset_in_page(gpa);
+
+	u8 arr[7] = {0x41, 0x81, 0xF9, 0x90, 0x65, 0x00, 0x00};
+	bool sussy = false;
+	for (u64 i = 0; i < 0x20; i++) {
+		if (!memcmp(hva + i, arr, sizeof(arr))) {
+			printk(KERN_ALERT "MR SUTER MATCHED ON CMP [RIP] -> 0x%llx\n", rip);
+			kvm_r9_write(vcpu, 0x6590);
+			sussy = true;
+		}
+	}
+
+	if (!sussy && r9 == 0x6590) {
+		printk(KERN_ALERT "MR SUTER MATCHED ON R9  [RIP] -> 0x%llx\n", rip);
+		kvm_r9_write(vcpu, 10);
+	}
+
+	kvm_vcpu_unmap(vcpu, &map, true);
+}
+
 void introspection_vmexit_callback(struct kvm_vcpu* vcpu) {
-//	u64 rip = kvm_rip_read(vcpu) - 0x20;
-//	u64 r9 = kvm_r9_read(vcpu);
-//
-//	struct kvm_host_map map;
-//	gpa_t gpa = kvm_mmu_gva_to_gpa_read(vcpu, rip, NULL);
-//	if (kvm_vcpu_map(vcpu, gpa_to_gfn(gpa), &map)) {
-//		printk(KERN_ALERT "vcpu map fail...\n");
-//		return;
-//	}
-//
-//	if (!map.hva) {
-//		printk(KERN_ALERT "mapped page invalid hva...\n");
-//		return;
-//	}
-//
-//	u8* hva = map.hva + offset_in_page(gpa);
-//
-//	u8 arr[7] = {0x41, 0x81, 0xF9, 0x90, 0x65, 0x00, 0x00};
-//	bool sussy = false;
-//	for (u64 i = 0; i < 0x20; i++) {
-//		if (!memcmp(hva + i, arr, sizeof(arr))) {
-//			printk(KERN_ALERT "MR SUTER MATCHED ON CMP [RIP] -> 0x%llx\n", rip);
-//			kvm_r9_write(vcpu, 0x6590);
-//			sussy = true;
-//		}
-//	}
-//
-//	if (!sussy && r9 == 0x6590) {
-//		printk(KERN_ALERT "MR SUTER MATCHED ON R9  [RIP] -> 0x%llx\n", rip);
-//		kvm_r9_write(vcpu, 10);
-//	}
-//
-//	kvm_vcpu_unmap(vcpu, &map, true);
+	anti_vm_real(vcpu);
 }
